a8e3b8b9c1166c5081a0fd6f2d24e8deb0dccd7a
Geraint <geraint@sixticks.com>
2025-08-26 15:58:27 +0100

    File Manager upload plugin

--- libs/custom/file-manager.js
+++ libs/custom/file-manager.js
@@ -2,6 +2,7 @@ class FileManager {
 	constructor(modal = true) {
 		this.isInit = false;
 		this.isModal = modal;
+		this.fileUploaded = false;
 
 		this.modalHtml =
 			`
@@ -42,7 +43,7 @@ class FileManager {
 		</div>`;
 
 		this.response = [],
-			this.currentPath = '';
+		this.currentPath = '';
 		this.breadcrumbsUrls = [];
 		this.filemanager = null;
 		this.breadcrumbs = null;
@@ -86,7 +87,7 @@ class FileManager {
 				$card.addClass('has-file is-folder');
 			} else if (type === 'gallery image' || type === 'image') {
 				$media.css('background-image', `url("${photoUi}")`).data('bgApplied', 1);
-				$card.addClass('has-file is-photo');
+				$card.addClass('has-file is-image');
 			} else if (type === 'document' || type === 'file') {
 				$media.css('background-image', `url("${documentUi}")`).data('bgApplied', 1);
 				$card.addClass('has-file is-file');
@@ -117,9 +118,9 @@ class FileManager {
 	getFolderIconUri() {
 		const svg = `
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
-			<path fill="#f4c542" d="M4 10a2 2 0 0 1 2-2h13l3 3h20a2 2 0 0 1 2 2v3H4v-6z"/>
-			<path fill="#f7d774" d="M4 16h40v20a4 4 0 0 1-4 4H8a4 4 0 0 1-4-4V16z"/>
-			<path fill="#d4a72c" d="M4 16h40v2H4z" opacity=".35"/>
+				<path fill="#f4c542" d="M4 10a2 2 0 0 1 2-2h13l3 3h20a2 2 0 0 1 2 2v3H4v-6z"/>
+				<path fill="#f7d774" d="M4 16h40v20a4 4 0 0 1-4 4H8a4 4 0 0 1-4-4V16z"/>
+				<path fill="#d4a72c" d="M4 16h40v2H4z" opacity=".35"/>
 			</svg>
 		`;
 		return `data:image/svg+xml;utf8,${encodeURIComponent(svg)}`;
@@ -128,11 +129,11 @@ class FileManager {
 	getDocumentIconUri() {
 		const svg = `
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
-			<path fill="#e9ecef" d="M8 2h20l12 12v32a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2z"/>
-			<path fill="#dee2e6" d="M28 2v12h12L28 2z"/>
-			<rect x="12" y="20" width="24" height="2" fill="#adb5bd"/>
-			<rect x="12" y="26" width="24" height="2" fill="#adb5bd"/>
-			<rect x="12" y="32" width="16" height="2" fill="#adb5bd"/>
+				<path fill="#e9ecef" d="M8 2h20l12 12v32a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2z"/>
+				<path fill="#dee2e6" d="M28 2v12h12L28 2z"/>
+				<rect x="12" y="20" width="24" height="2" fill="#adb5bd"/>
+				<rect x="12" y="26" width="24" height="2" fill="#adb5bd"/>
+				<rect x="12" y="32" width="16" height="2" fill="#adb5bd"/>
 			</svg>
 		`;
 		return `data:image/svg+xml;utf8,${encodeURIComponent(svg)}`;
@@ -140,12 +141,12 @@ class FileManager {
 
 	getPhotoPlaceholderUri() {
 		const svg = `
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 48">
-			<rect x="1.5" y="1.5" width="61" height="45" rx="6" ry="6" fill="#f1f3f5" stroke="#dee2e6"/>
-			<circle cx="46" cy="16" r="5" fill="#ced4da"/>
-			<path d="M8 38l12-14 9 10 10-12 17 16H8z" fill="#adb5bd"/>
-			<rect x="1.5" y="1.5" width="61" height="45" rx="6" ry="6" fill="none" stroke="#ced4da"/>
-		</svg>`;
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 48">
+				<rect x="1.5" y="1.5" width="61" height="45" rx="6" ry="6" fill="#f1f3f5" stroke="#dee2e6"/>
+				<circle cx="46" cy="16" r="5" fill="#ced4da"/>
+				<path d="M8 38l12-14 9 10 10-12 17 16H8z" fill="#adb5bd"/>
+				<rect x="1.5" y="1.5" width="61" height="45" rx="6" ry="6" fill="none" stroke="#ced4da"/>
+			</svg>`;
 		return `data:image/svg+xml;utf8,${encodeURIComponent(svg)}`;
 	}
 
@@ -268,9 +269,23 @@ class FileManager {
 					self.storeFolderId(folder_id);
 				}
 
+				// initialise the file uploader
+				this.initUploader();
+
 				// run the initial lazy load function
 				this.lazyLoadImages(self.container);
 				this.setupListeners();
+
+				// select the parent container of all the cards
+				const container = $('.card-wrapper');
+				if (container.length) {
+
+					// find the first card that is not a folder
+					const firstFile = $('.card-wrapper .card-wrap:not(:has(.is-folder))').first();
+					if (firstFile.length) {
+						$('<div>').addClass('flex-break').insertBefore(firstFile);
+					}
+				}
 
 				// when the user scrolls, run the lazy load function
 				const $scrollTargets = $(window).add($bodyEl);
@@ -282,6 +297,120 @@ class FileManager {
 			}
 		}).catch(function (error) {
 			console.error("Error loading file manager:", error);
+		});
+	}
+
+	initUploader() {
+		const self = this;
+
+		// toggle the visibility of the upload view
+		$('#show-upload-btn').on('click', function() {
+			$('#file-upload-view').addClass('is-visible');
+		});
+		$('#back-to-manager-btn').on('click', function() {
+			$('#file-upload-view').removeClass('is-visible');
+			if(self.fileUploaded) {
+				self.fileUploaded = false;
+				self.loadFolder();
+			}
+		});
+
+		// register plugins
+		FilePond.registerPlugin(FilePondPluginImagePreview);
+		FilePond.registerPlugin(FilePondPluginImageResize);
+		FilePond.registerPlugin(FilePondPluginImageValidateSize);
+		FilePond.registerPlugin(FilePondPluginImageTransform);
+
+		// turn input element(s) into a filepond
+		$('.st-uploader').filepond();
+
+		// initialise the filepond with custom server options
+		$('.st-uploader').filepond({
+			allowMultiple: true,
+			allowImageValidateSize: true,
+			allowImageResize: true,
+			imageResizeTargetWidth: 1920,
+			imageResizeTargetHeight: 1920,
+			imageResizeMode: '	',
+			imageResizeUpscale: false,
+			imageValidateSizeMaxWidth: 1920,
+			imageValidateSizeMaxHeight: 1920,
+			server: {
+				process: (fieldName, file, metadata, load, error, progress, abort) => {
+
+					// build the endpoint URL
+					const urlParams = new URLSearchParams(window.location.search);
+					const i = urlParams.get('i');
+					const w = urlParams.get('w');
+					const rootUrl = window.location.origin + '/';
+					const endpoint = rootUrl + 'process/vvveb.php';
+
+					// get the folder id
+					const folder_id = this.getFolderId();
+
+					// build the form data
+					const formData = new FormData();
+					formData.append('i', i);
+					formData.append('w', w);
+					formData.append('a', 'fileUploaded');
+					formData.append('folder', folder_id);
+					formData.append(fieldName, file, file.name);
+
+					// run the ajax request
+					const request = $.ajax({
+						url: endpoint,
+						type: 'POST',
+						data: formData,
+						processData: false,
+						contentType: false,
+						xhr: function () {
+
+							// allow tracking of the upload progress
+							const xhr = new window.XMLHttpRequest();
+							xhr.upload.addEventListener('progress', e => {
+								if (e.lengthComputable) {
+									progress(true, e.loaded, e.total);
+								}
+							}, false);
+							return xhr;
+						},
+						success: function(response) {
+							self.fileUploaded = true;
+							load(response);
+						},
+						error: function(jqXHR, textStatus, errorMessage) {
+
+							// on error, call the 'error' callback with an error message
+							console.error('Upload failed:', errorMessage);
+							error('Upload failed');
+						}
+					});
+
+					// return an object with an abort method
+					return {
+						abort: () => {
+							request.abort();
+							abort();
+						}
+					};
+				},
+				revert: (uniqueFileId, load, error) => {
+
+					// for the revert action, we can use the normal process as no file is included
+					stAjaxCall('fileRevert', { id: uniqueFileId }, 'POST')
+						.then(response => {
+							load();
+						})
+						.catch(() => {
+							error('Could not revert file');
+						});
+				}
+			}
+		});
+
+		// listen for addfile event
+		$('.st-uploader').on('FilePond:addfile', function(e) {
+			console.log('file added event', e);
 		});
 	}
 }
